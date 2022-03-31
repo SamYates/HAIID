16ee04a197e8c3d1177204deed56b49cb4bbae8e
Aidan-WW <44363391+Aidan-WW@users.noreply.github.com>
2022-03-31 10:01:14 +0100

    fixed diet dir

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 import spoonacular
 
-API_KEY = "a80ce6a267f14f4f86a64efe027f6495"
+#API_KEY = "a80ce6a267f14f4f86a64efe027f6495"
 
 app = Flask(__name__)
-api = spoonacular.API(API_KEY)
+#api = spoonacular.API(API_KEY)
 
 
 
@@ -12,7 +12,7 @@ api = spoonacular.API(API_KEY)
 def home_view():
     return "<h1>HAIID peep PEEPS!</h1>"
 
-@app.route("/diet=:dietstring")
+@app.route("/diet=:dietstring/")
 def get_meal_plan(dietstring):
     response = api.generate_meal_plan(diet=dietstring)
     data = response.json()
