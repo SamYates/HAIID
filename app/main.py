a9d436c94610bb50fe0388787fddadfee7db4413
Aidan-WW <44363391+Aidan-WW@users.noreply.github.com>
2022-03-31 10:29:15 +0100

    something

--- app/main.py
+++ app/main.py
@@ -27,5 +27,5 @@ def recipe_search(query):
     response = api.search_recipes_complex(query)
     data = response.json()
     img = data['results'][0]['image']
-    return "hi"
+    #return "hi"
     return send_file(img, mimetype='image/gif')
