5552f35375e1254858da43f3b69036d31c5a8cc1
Sam Yates <2samyates@gmail.com>
2022-04-08 19:50:07 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,14 +16,14 @@ api = spoonacular.API(API_KEY)
 #UPLOAD_FOLDER = "/upload_images" #change to host directory
 #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-classifier = keras.models.load_model('classifierModel')
+#classifier = keras.models.load_model('classifierModel')
 
-strawberryModel = keras.models.load_model('strawberryModel')
-onionModel = keras.models.load_model('onionModel')
-carrotModel = keras.models.load_model('carrotModel')
-beetrootModel = keras.models.load_model('beetrootModel')
-cucumberModel = keras.models.load_model('cucumberModel')
-tomatoModel = keras.models.load_model('tomatoModel')
+#strawberryModel = keras.models.load_model('strawberryModel')
+#onionModel = keras.models.load_model('onionModel')
+#carrotModel = keras.models.load_model('carrotModel')
+#beetrootModel = keras.models.load_model('beetrootModel')
+#cucumberModel = keras.models.load_model('cucumberModel')
+#tomatoModel = keras.models.load_model('tomatoModel')
 #potatoModel = keras.models.load_model('potatoModel')
 #pepperModel = keras.models.load_model('pepperModel')
 
