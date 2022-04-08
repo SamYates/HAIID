f3058470d002b8e7312e5f52f1207dfa5b92e5c1
Sam Yates <2samyates@gmail.com>
2022-04-08 16:50:59 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,33 @@
-from flask import Flask
-from flask import send_file
-from flask import request
+from flask import Flask, send_file, request
 from tensorflow import keras
 import spoonacular
 import cv2
 import numpy as np
 import os
-import werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
 
 API_KEY = "a80ce6a267f14f4f86a64efe027f6495"
 
 app = Flask(__name__)
 api = spoonacular.API(API_KEY)
 
+home_dir = os.path.expanduser("~")
+UPLOAD_FOLDER = os.path.join(home_dir, "Documents/University/Y3/HAIID/repo/HAIID/app/upload_images") #change to host directory
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+classifier = keras.models.load_model('classifierModel')
+
+strawberryModel = keras.models.load_model('strawberryModel')
+onionModel = keras.models.load_model('onionModel')
+carrotModel = keras.models.load_model('carrotModel')
+beetrootModel = keras.models.load_model('beetrootModel')
+cucumberModel = keras.models.load_model('cucumberModel')
+tomatoModel = keras.models.load_model('tomatoModel')
+#potatoModel = keras.models.load_model('potatoModel')
+#pepperModel = keras.models.load_model('pepperModel')
+
+modelArray = [beetrootModel, carrotModel, cucumberModel, onionModel, strawberryModel, tomatoModel] #potatoModel pepperModel
+classNames = ["beetroot", "carrot", "cucumber", "onion", "strawberry", "tomato"] #potato pepper
 
 @app.route("/")
 def home_view():
@@ -38,9 +53,37 @@ def recipe_search(query):
     return "<h1>" + str(img) + "</h1>"
 
 
-@app.route("/get_growth_stage/<string:image>/")
-def nn(image):
-    return "RIPE TOMATO"
+@app.route("/get_growth_stage", methods = ['POST'])
+def get_growth_stage():
+    new_file = request.files['image']
+    file_name = secure_filename(new_file.filename)
+    new_file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+    image = np.array([cv2.resize(cv2.imread('upload_images/' + file_name)/255, (224, 224), interpolation = cv2.INTER_AREA)])
+    
+    imageClass = np.argmax(classifier.predict(image)[0])
+
+    model = modelArray[imageClass]
+
+    prediction = model.predict(image)
+
+    if imageClass == 0:
+        progress = (1.5*prediction[0][0] + 4*prediction[0][1] + 7*prediction[0][2])/7
+    elif imageClass == 1:
+        progress = (2*prediction[0][0] + 6.5*prediction[0][1]+ 10*prediction[0][2])/10
+    elif imageClass == 2:
+        progress = (1.5*prediction[0][0] + 4*prediction[0][1] + 7*prediction[0][2] + 9*prediction[0][3])/9
+    elif imageClass == 3:
+        progress = (1.5*prediction[0][0] + 6.5*prediction[0][1] + 11*prediction[0][2])/11
+    elif imageClass == 4:
+        progress = (prediction[0][0] + 2.5*prediction[0][1]+4*prediction[0][2]+6*prediction[0][3])/6
+    elif imageClass == 5:
+        progress = (prediction[0][0] + 3*prediction[0][1] + 6.5*prediction[0][2] + 8*prediction[0][3])/8
+    #elif imageClaass == 6:
+    #    progress = (##*prediction[0][0] + ##*prediction[0][1] + ##*prediction[0][2])/##
+    #elif imageClaass == 7:
+    #    progress = (##*prediction[0][0] + ##*prediction[0][1] + ##*prediction[0][2])/##
+
+    return str(progress)
 
 
 @app.route("/get_recipes")
@@ -85,7 +128,6 @@ def get_recipes():
 ##    diet = request.args.get('diet', default='None', type = str)
 ##    user_ingredients = request.args.get('userIngredients',default='flour,egg,milk,banana,honey,golden syrup,butter', type=str)
     
-
 
 
 #DEBUG MODE!!
